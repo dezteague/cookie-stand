d5546259ee37a30e573dcd3ce8de4972813d5972
Deziree Teague <dez.teague@gmail.com>
2018-11-06 12:34:37 -0800

    added use strict to js file

--- apps.js
+++ apps.js
@@ -1,4 +1,4 @@
-'strict';
+'use strict';
 
 var stores = [];//this array is left empty so that stores can be added later
 var hoursOfOps = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
